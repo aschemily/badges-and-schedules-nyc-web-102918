2b014b2f9f5b6d74c2f176c789e025eaa63258db
Emily Asch <e.asch@me.com>
2018-10-11 21:50:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,4 +26,7 @@ badge_messages = batch_badge_creator(speakers)
 badge_messages.each do |message|
   puts message
   end
+  room_messages = assign_rooms(speakers)
+  room_messages.each do |message| puts message
+end
 end
